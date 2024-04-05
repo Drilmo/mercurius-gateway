f16a0f9b1636b5424129024eb824f6fc651da3a5
Drilmo <31286436+Drilmo@users.noreply.github.com>
2024-04-05 08:51:51 +0000

    chore: :construction: add exemple-2 for reproductible error

---
+++ exemple-2/plugins/links.js
@@ -0,0 +1,96 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const createFederationNode = require('../createFederationNode.js')
+
+const links = {
+  l1: {
+    lid: 'l1',
+    pid: 'p1',
+    authorId: 'u1'
+  },
+  l2: {
+    lid: 'l2',
+    pid: 'p2',
+    authorId: 'u2'
+  },
+  l3: {
+    lid: 'l3',
+    pid: 'p3',
+    authorId: 'u1'
+  },
+  l4: {
+    lid: 'l4',
+    pid: 'p4',
+    authorId: 'u2'
+  },
+  l5: {
+    lid: 'l5',
+    pid: 'p1',
+    authorId: 'u2'
+  },
+  l6: {
+    lid: 'l6',
+    pid: 'p3',
+    authorId: 'u2'
+  }
+}
+
+module.exports = fp(
+  async fastify => {
+    const schema = `
+      #graphql
+      type Post @key(fields: "pid") @extends {
+        pid: ID! @external
+        title: String @external
+        authors: [User]
+        numberOfAuthors: Int @requires(fields: "pid title")
+      }
+
+      type User @key(fields: "id") @extends {
+        id: ID! @external
+        name: String @external
+        posts: [Post]
+        numberOfPosts: Int @requires(fields: "id name")
+      }
+
+    `
+
+    const resolvers = {
+      Post: {
+        authors: post => {
+          return Object.values(links).filter(l => l.pid === post.pid).map(link => {
+            return {
+              __typename: 'User',
+              id: link.authorId
+            }
+          })
+        },
+        numberOfAuthors: post => {
+          return Object.values(links).filter(l => l.pid === post.pid).length
+        }
+      },
+      User: {
+        posts: user => {
+          return Object.values(links).filter(l => l.authorId === user.id).map(link => {
+            return {
+              __typename: 'Post',
+              pid: link.pid
+            }
+          })
+        },
+        numberOfPosts: user => {
+          return Object.values(links).filter(l => l.authorId === user.id).length
+        }
+      }
+    }
+
+    fastify.log.info('Initializing node Node 3 on port 4003')
+    await createFederationNode('Node 3', schema, resolvers, 4003)
+    fastify.log.info('Started node Node 3 on port 4003')
+  },
+  {
+    name: 'node-3',
+    dependencies: []
+  }
+)
---
+++ exemple-2/plugins/post.js
@@ -0,0 +1,88 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const createFederationNode = require('../createFederationNode.js')
+
+const posts = {
+  p1: {
+    pid: 'p1',
+    title: 'Post 1',
+    content: 'Content 1'
+  },
+  p2: {
+    pid: 'p2',
+    title: 'Post 2',
+    content: 'Content 2'
+  },
+  p3: {
+    pid: 'p3',
+    title: 'Post 3',
+    content: 'Content 3'
+  },
+  p4: {
+    pid: 'p4',
+    title: 'Post 4',
+    content: 'Content 4'
+  }
+}
+
+module.exports = fp(
+  async fastify => {
+    const schema = `
+      #graphql
+      type Post @key(fields: "pid") {
+        pid: ID!
+        title: String
+        content: String
+      }
+
+      type Query @extends {
+        topPosts(count: Int): [Post]
+      }
+
+      extend type Mutation {
+        createPost(post: PostInput!): Post
+        updateHello: String
+      }
+
+      input PostInput {
+        title: String!
+        content: String!
+        authorId: String!
+      }
+    `
+
+    const resolvers = {
+      Post: {
+        __resolveReference: post => {
+          return posts[post.pid]
+        }
+      },
+      Query: {
+        topPosts: (root, { count = 2 }) => Object.values(posts).slice(0, count)
+      },
+      Mutation: {
+        createPost: (root, { post }) => {
+          const pid = `p${Object.values(posts).length + 1}`
+
+          const result = {
+            pid,
+            ...post
+          }
+          posts[pid] = result
+
+          return result
+        },
+        updateHello: () => 'World'
+      }
+    }
+
+    fastify.log.info('Initializing node Node 2 on port 4002')
+    await createFederationNode('Node 2', schema, resolvers, 4002)
+    fastify.log.info('Started node Node 2 on port 4002')
+  },
+  {
+    name: 'node-2',
+    dependencies: []
+  }
+)
